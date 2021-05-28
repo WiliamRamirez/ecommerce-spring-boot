1eb881cb88cd73f3994bd0182a86568950bc6eb1
WiliamRamirez <wiliam.ramirez.27@unsch.edu.pe>
2021-05-28 15:05:48 -0500

    first commit

---
+++ src/main/java/com/luv2code/ecommerce/entity/ProductCategory.java
@@ -0,0 +1,30 @@
+package com.luv2code.ecommerce.entity;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "product_category")
+// Data --know bug
+@Getter
+@Setter
+public class ProductCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "categoryName")
+    private String categoryName;
+
+    @Column(name = "products")
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private Set<Product> products;
+
+
+}
